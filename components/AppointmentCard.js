20171f04ba1abe68461e152871cb683c668d49d0
Jay Macpherson <jaymacpherson93@gmail.com>
2019-02-21 05:07:01 +0000

    Refactor AppointmentCard.js
    Broken down into smaller stylable components that better reflect the component hierachy rather than spamming view tags.

--- components/AppointmentCard.js
+++ components/AppointmentCard.js
@@ -1,46 +1,62 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
-import moment from "moment";
-//TODO: import {AppointmentCardStyle} from StyleSheet path
+import moment from 'moment';
+import styles from '../StyleSheet';
+import { Ionicons } from '@expo/vector-icons';
+import {
+    AppointmentHeader,AppointmentBody, AppointmentDetail,
+    AppointmentTimer,AppointmentTitle, AppointmentContainer
+} from './appointment_elements';
 
 export const AppointmentCard = ({appointment}) => {
     const {appointmentType, startTime, location} = appointment;
     const appointmentDate = moment(startTime);
+    const {appointmentTypeText, appointmentTitleText, appointmentTimerText} = styles;
     
     const doNothing = () => {
 
     };
 
     return (
-        <View>
-            <View>
-                <Text>Appointment Details</Text>
-                <Text>{appointmentType}</Text>
-                <Text>{appointmentDate.fromNow()}</Text>
-            </View>
-            
-            <View>
-                <Text>{location}</Text>
-                <Text>{appointmentDate.format('MMMM Do YYYY, h:mm a')}</Text>
-            </View>
+        <AppointmentContainer>
+            <AppointmentHeader>
+                <AppointmentTitle>
+                    <Text style = { appointmentTitleText }>Appointment details:</Text>
+                    <Text style = { appointmentTypeText }>{appointmentType}</Text>
+                </AppointmentTitle>
+                <AppointmentTimer>
+                    <Text style = { appointmentTimerText }>{appointmentDate.fromNow()}</Text>
+                </AppointmentTimer>
+            </AppointmentHeader>
 
-            <View>
-                <Button title="Preparation checklist" onPress = {doNothing}/>
-            </View>
+            <AppointmentBody>
+                <AppointmentDetail>
+                    <Ionicons name = "md-pin" size = {30} color = "black"></Ionicons>
+                    <Text onPress={() => Linking.openURL('http://google.com')}>{location}</Text>
+                </AppointmentDetail>
+                <AppointmentDetail>
+                <Ionicons name = "ios-clock" size = {30} color = "black"></Ionicons>
+                    <Text>{appointmentDate.format('MMMM Do YYYY, h:mm a')}</Text>
+                </AppointmentDetail>
 
-            <View>
-                <Button title="What to expect" onPress = {doNothing}/>
-            </View>
+                <View>
+                    <Button title='Preparation checklist' onPress = {doNothing}/>
+                </View>
 
-            <Text>
-                Can't make it to your appointment? It's important to
-                let us know so we can offer your slot to somebody else:
-            </Text>
+                <View>
+                    <Button title='What to expect' onPress = {doNothing}/>
+                </View>
 
-            <View>
-                <Button title="Cancel or change appointment" onPress = {doNothing}/>
-            </View>
-        </View>
+                <Text>
+                    Can't make it to your appointment? It's important to
+                    let us know so we can offer your slot to somebody else:
+                </Text>
+
+                <View>
+                    <Button title='Cancel or change appointment' onPress = {doNothing}/>
+                </View>
+            </AppointmentBody>
+        </AppointmentContainer>
     );
 }
 

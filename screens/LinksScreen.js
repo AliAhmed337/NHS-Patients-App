84284bf0af75b66a241c31dd58919d1fbeaf3eba
k1763447 <ahmed.1.ali@kcl.ac.uk>
2019-02-21 21:12:32 +0000

    Link linksScreen to PreparationScreen
    Add button that moves user to preparation screen

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, Button } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { AppointmentCard } from '../components/AppointmentCard';
 
@@ -17,6 +17,7 @@ export default class LinksScreen extends React.Component {
         <AppointmentCard appointment = {appointments.first}/>
         <AppointmentCard appointment = {appointments.second}/>
         <AppointmentCard appointment = {appointments.third}/>
+        <Button title="a VERY SPECIAL THING" onPress={() => this.props.navigation.navigate('Preparation')}/> 
       </ScrollView>
     );
   }

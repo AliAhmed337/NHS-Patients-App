8cfbf2859513769407d43b549266562e512d2052
Jay Macpherson <jaymacpherson93@gmail.com>
2019-02-21 05:09:33 +0000

    Modified LinksScreen.js
    Included an appointment loader as test for when an api is introduced. Can be extended to provide preparation info.

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import { AppointmentCard } from '../components/AppointmentCard';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -13,10 +14,34 @@ export default class LinksScreen extends React.Component {
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
         <ExpoLinksView />
+        <AppointmentCard appointment = {appointments.first}/>
+        <AppointmentCard appointment = {appointments.second}/>
+        <AppointmentCard appointment = {appointments.third}/>
       </ScrollView>
     );
   }
 }
+
+const appointments = {
+  first: {
+    appointmentType: 'Heart CT Scan ',
+    startTime: "2019-03-04T16:35:01",
+    location: 'St Thomas Hospital',
+  },
+
+  second: {
+    appointmentType: 'Digital Rectal Exam ',
+    startTime: "2019-08-02T10:40",
+    location: 'St James Hospital',
+  },
+
+  third: {
+    appointmentType: 'Appendix CT Scan ',
+    startTime: "2021-09-11T18:10",
+    location: 'Northwick Park Hospital',
+  },
+    
+};
 
 const styles = StyleSheet.create({
   container: {

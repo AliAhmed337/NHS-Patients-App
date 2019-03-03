e15b0cad565372f9ff5cd74bcc5a1c141d0f36cf
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-03 17:12:58 +0000

    Update AuthNavigator.js

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -1,8 +1,9 @@
 import AuthenticationScreen from '../screens/AuthenticationScreen';
 
-const AuthStack = createStackNavigator({
+export default AuthStack = createStackNavigator({
     AuthMain: AuthenticationScreen,
-  });
+    QRAuth: QRAuthenticationScreen
+  }); 
 
   
 

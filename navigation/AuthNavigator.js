fc0b3e66af5abbca51ed4096bc48dbdd76719b2e
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-06 22:09:08 +0000

    Add manual auth to the NavStack

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -1,10 +1,12 @@
 import { createStackNavigator } from 'react-navigation';
 import AuthenticationScreen from '../screens/AuthenticationScreen';
 import QRAuthenticationScreen from '../screens/QRAuthenticationScreen';
+import ManualAuthenticationScreen from '../screens/ManualAuthenticationScreen';
 
 const AuthStack = createStackNavigator({
     AuthMain: AuthenticationScreen,
     QRAuth: QRAuthenticationScreen,
+    ManualAuth: ManualAuthenticationScreen,
   });
 
 export default AuthStack;

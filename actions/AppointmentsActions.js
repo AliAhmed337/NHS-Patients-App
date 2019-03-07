fd19e650846c31f744fd26f055e0977a8e8c23b5
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-07 04:55:48 +0000

    Implement static json request via actions

---
+++ actions/AppointmentsActions.js
@@ -0,0 +1,20 @@
+import {
+    APPOINTMENTS_REQUESTED,APPOINTMENTS_RETRIEVED,CLEAR_APPOINTMENTS
+} from './types';
+
+const url = "https://static.dominicswaine.com/nhs-app/appointments.json";
+
+export const requestAppointments = () => {
+
+    return (dispatch) => {
+        dispatch({type: CLEAR_APPOINTMENTS});
+        dispatch({type: APPOINTMENTS_REQUESTED});
+
+        fetch(url, {headers: {'Cache-Control': 'no-cache'}})
+        .then((response) => response.json())
+        .then((responseJson) => {
+            dispatch({type: APPOINTMENTS_RETRIEVED, responseJson});
+        })
+        .catch((error) => console.error(error));
+    }
+};

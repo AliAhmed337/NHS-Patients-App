0f4f17ae77a98ee8490d8da829f0b6d8d2c9ba19
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-09 05:29:18 +0000

    Modify ManualAuth for redux

--- screens/ManualAuthenticationScreen.js
+++ screens/ManualAuthenticationScreen.js
@@ -1,33 +1,49 @@
 import React from 'react';
-import {Text,View,TextInput} from 'react-native';
+import {Text,View,TextInput, ActivityIndicator, Keyboard} from 'react-native';
+import { connect } from 'react-redux';
+import { passphraseChanged, verifyUser } from '../actions';
 import Button from '../components/common/Button';
-import Header from '../components/common/ScreenHeader';
 
-
-export default class ManualAuthenticationScreen extends React.Component {
+class ManualAuthenticationScreen extends React.Component {
     static navigationOptions = {
         title: 'Enter details manually',
     };
-    constructor(props) {
-        super(props);
-        this.state = { text: '' };
+
+    _onPassphraseChange(text) {
+        this.props.passphraseChanged(text);
+    }
+
+    _onSubmitPress(){
+        const {passphrase, verifyUser} = this.props;
+        Keyboard.dismiss();
+        verifyUser(passphrase);
+    }
+
+    _renderForm(){
+        return (
+            <View>
+                <Text>Enter your passphrase:</Text>
+            <TextInput
+                placeholder = "example: gooseberry1034"
+                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                onChangeText={(text) => this._onPassphraseChange(text)}
+                value={this.props.passphrase}/>
+            <Button onPress = {this._onSubmitPress()}>{this.props.loading ? <ActivityIndicator/> : 'Submit'}</Button>
+                <Text>You'll find this on your appointment confirmation {"\n"}
+                    letter or text message</Text>
+            </View>
+        );
     }
 
     render(){
         const {navigate} = this.props.navigation;
-        return (
-            <View>
-                <Text>Enter your reference number:</Text>
-            <TextInput
-                placeholder = "input"
-                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-                onChangeText={(text) => this.setState({text})}
-                value={this.state.text}/>
-            <Button>Submit</Button>
-                <Text>You'll find this on your appointment confirmation {"\n"}
-                    letter or text message</Text>
-            </View>
-        )
+        return(this.props.user && this.props.user.exists ? navigate('Main') : this._renderForm());        
     }
 }
 
+const mapStateToProps = ({ manRed }) => {
+  const { passphrase, user, loading } = manRed;
+  return { passphrase, user,  loading };
+}
+
+export default connect(mapStateToProps, {passphraseChanged, verifyUser})(ManualAuthenticationScreen);

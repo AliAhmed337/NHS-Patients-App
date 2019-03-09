3396ff7257bbef7ec2a5b773f376027e04f0d32b
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-09 05:27:58 +0000

    Update with QR and Auth reducers

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import AppointmentsReducer from './AppointmentsReducer';
+import QRAuthReducer from './QRAuthReducer';
+import ManualAuthReducer from './ManualAuthReducer';
 
 export default combineReducers({
-    appointmentsRed: AppointmentsReducer
+    appointmentsRed: AppointmentsReducer,
+    qrRed: QRAuthReducer,
+    manRed: ManualAuthReducer
 });

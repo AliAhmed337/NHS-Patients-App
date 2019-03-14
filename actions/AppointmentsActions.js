7c5cb80f53d143e033c5acb12b4d881617f94046
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-14 04:34:04 +0000

    Implement appointments fetch API

--- actions/AppointmentsActions.js
+++ actions/AppointmentsActions.js
@@ -4,19 +4,26 @@ import {
     CLEAR_APPOINTMENTS
 } from './types';
 
-const url = "https://static.dominicswaine.com/nhs-app/appointments.json";
+import { AsyncStorage } from 'react-native';
 
-export const requestAppointments = () => {
+const url = "https://nhs.hallsy.io/api/v1/appointments";
+
+export const requestAppointments = async () => {
+
+    const userToken = await AsyncStorage.getItem('userToken');
 
     return (dispatch) => {
         dispatch({type: CLEAR_APPOINTMENTS});
         dispatch({type: APPOINTMENTS_REQUESTED});
 
-        fetch(url, {headers: {'Cache-Control': 'no-cache'}})
+        fetch(url, {headers: {
+            'Cache-Control': 'no-cache',
+            'X-API-KEY': userToken
+        }})
         .then((response) => response.json())
-        .then((responseJson) => {
-            console.log('Appointments retrieved');
-            dispatch({type: APPOINTMENTS_RETRIEVED, payload: responseJson.appointments});
+            .then((responseJson) => {
+                console.log('Appointments retrieved');
+                dispatch({type: APPOINTMENTS_RETRIEVED, payload: responseJson.appointments});
         })
         .catch((error) => console.error(error));
     }

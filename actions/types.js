9c8a539188030b494924cfe3ebfee32bfe925df3
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-14 04:47:43 +0000

    Update types.js

--- actions/types.js
+++ actions/types.js
@@ -2,7 +2,8 @@ export const APPOINTMENTS_REQUESTED = 'appointments_requested';
 export const APPOINTMENTS_RETRIEVED = 'appointments_retrieved';
 export const CLEAR_APPOINTMENTS = 'clear_appointments';
 
-export const  PASSPHRASE_CHANGED = 'passphrase_changed';
+export const PASSPHRASE_CHANGED = 'passphrase_changed';
+export const CAMERA_PERMISSION_REQUESTED = 'camera_permission_requested';
 export const VERIFY_USER = 'verify_user';
 export const USER_VERIFY_SUCCESS = 'user_verify_success';
 export const USER_VERIFY_FAIL = 'user_verify_fail';

054e214740e468cd118e5f680a8c7ea0f837e176
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-24 00:24:43 +0000

    Prepare api fetch

--- actions/PreparationActions.js
+++ actions/PreparationActions.js
@@ -4,19 +4,25 @@ import {
     CLEAR_PREPARATIONS
 } from './types';
 
-const PREPARATION_ENDPOINT = "https://nhs.hallsy.io/api/v1/";
 
-export const requestPreparations = () => {
+export const requestPreparations = (PREPARATION_ENDPOINT, userToken) => {
 
     return (dispatch) => {
+        console.log('getting preparations');
         dispatch({type: CLEAR_PREPARATIONS});
         dispatch({type: PREPARATIONS_REQUESTED});
 
-        fetch(PREPARATION_ENDPOINT, {headers: {'Cache-Control': 'no-cache'}})
+        fetch(PREPARATION_ENDPOINT, {
+            method: 'GET',
+            headers: {
+                'Cache-Control': 'no-cache',
+                'X-API-KEY': userToken
+
+            }})
         .then((response) => response.json())
         .then((responseJson) => {
-            console.log('PREPARATIONS retrieved');
-            dispatch({type: PREPARATIONS_RETRIEVED, payload: responseJson.PREPARATIONS});
+            console.log('preparations retrieved for endpoint: ' + PREPARATION_ENDPOINT);
+            dispatch({type: PREPARATIONS_RETRIEVED, payload: responseJson.preparations});
         })
         .catch((error) => console.error(error));
     }

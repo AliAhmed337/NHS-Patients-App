fb985f47f51823922b7a1126ac05067f3a7444fa
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-24 04:11:20 +0000

    Update types.js

--- actions/types.js
+++ actions/types.js
@@ -3,7 +3,11 @@ export const APPOINTMENTS_RETRIEVED = 'appointments_retrieved';
 export const CLEAR_APPOINTMENTS = 'clear_appointments';
 
 export const PREPARATIONS_REQUESTED = 'preparation_requested';
-export const PREPARATIONS_RETRIEVED = 'preparation_retrieved';
+export const PREPARATIONS_COMPLETE = 'preparation_complete';
+export const TIMELINE_CREATED = 'timeline_created';
+export const GUIDELINE_CREATED = 'guideline_created'; 
+export const DIET_CREATED = 'diet_created';
+export const MENU_CREATED = 'menu_created';
 export const CLEAR_PREPARATIONS = 'clear_preparations';
 
 export const CAMERA_PERMISSION_REQUESTED = 'camera_permission_requested';

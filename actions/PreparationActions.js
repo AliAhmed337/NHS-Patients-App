ecea145e41a9f0ddc537ae12cd9e2c444abaed7a
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-24 04:10:55 +0000

    Expand Prep tasks
    break down the different types of preparation and allocate them to their respective lists

--- actions/PreparationActions.js
+++ actions/PreparationActions.js
@@ -1,7 +1,6 @@
 import {
-    PREPARATIONS_REQUESTED,
-    PREPARATIONS_RETRIEVED,
-    CLEAR_PREPARATIONS
+    PREPARATIONS_REQUESTED, PREPARATIONS_COMPLETE, CLEAR_PREPARATIONS,
+    TIMELINE_CREATED, GUIDELINE_CREATED, DIET_CREATED, MENU_CREATED
 } from './types';
 
 
@@ -22,7 +21,12 @@ export const requestPreparations = (PREPARATION_ENDPOINT, userToken) => {
         .then((response) => response.json())
         .then((responseJson) => {
             console.log('preparations retrieved for endpoint: ' + PREPARATION_ENDPOINT);
-            dispatch({type: PREPARATIONS_RETRIEVED, payload: responseJson.preparations});
+            const prep = responseJson.preparatoryTasks;
+
+            // grab the data, split them, and reformat them to display in their relevant sections.
+
+
+            dispatch({type: PREPARATIONS_COMPLETE});
         })
         .catch((error) => console.error(error));
     }

62c84587444fad1fb5e3ba62110108540c56b80d
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-24 04:14:01 +0000

    Create lists for multiple preps

--- reducers/PreparationsReducer.js
+++ reducers/PreparationsReducer.js
@@ -1,9 +1,13 @@
 import {
-    PREPARATIONS_REQUESTED,PREPARATIONS_RETRIEVED,CLEAR_PREPARATIONS
+    PREPARATIONS_REQUESTED, PREPARATIONS_COMPLETE, CLEAR_PREPARATIONS,
+    TIMELINE_CREATED, GUIDELINE_CREATED, DIET_CREATED, MENU_CREATED
 } from '../actions/types';
 
 const INITIAL_STATE = {
-    preparations: [],
+    timelinePrep: [],
+    guidelinePrep: [],
+    dietPrep: [],
+    menuPrep: [],
     loading: false,
 };
 
@@ -11,16 +15,40 @@ export default (state = INITIAL_STATE, action) => {
 
     switch (action.type) {
         case PREPARATIONS_REQUESTED:
-        console.log('PREPARATIONS are being retrieved');
+            console.log('PREPARATIONS are being retrieved');
             return {...state, loading: true};
         
-        case PREPARATIONS_RETRIEVED:
-        console.log('PREPARATIONS have been retrieved: ' + action.payload);
+        case TIMELINE_CREATED:
+            console.log('timeline has been updated: ' + action.payload);
             return {
                 ...state, 
-                PREPARATIONS: action.payload, 
-                loading: false
+                timelinePrep: action.payload, 
             };
+
+        case GUIDELINE_CREATED:
+            console.log('guideline has been updated: ' + action.payload);
+            return {
+                ...state, 
+                guidelinePrep: action.payload, 
+            };
+
+        case DIET_CREATED:
+            console.log('diet has been updated: ' + action.payload);
+            return {
+                ...state, 
+                dietPrep: action.payload, 
+            };
+
+        case MENU_CREATED:
+            console.log('diet has been updated: ' + action.payload);
+            return {
+                ...state, 
+                menuPrep: action.payload, 
+            };
+
+        case PREPARATIONS_COMPLETE:
+            console.log('preparations have completed and all info is split');
+            return {...state, loading: false};
 
         case CLEAR_PREPARATIONS:
             return {...INITIAL_STATE};

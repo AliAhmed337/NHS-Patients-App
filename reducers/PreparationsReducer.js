b6bbff2c9b2a1ac641ccc3f422ea6b3d600d9103
Jay Macpherson <jaymacpherson93@gmail.com>
2019-03-26 06:26:12 +0000

    Handle object passing in prep

--- reducers/PreparationsReducer.js
+++ reducers/PreparationsReducer.js
@@ -1,11 +1,11 @@
 import {
-    PREPARATIONS_REQUESTED, PREPARATIONS_COMPLETE, CLEAR_PREPARATIONS,
-    TIMELINE_CREATED, GUIDELINE_CREATED, DIET_CREATED, MENU_CREATED
+    PREPARATIONS_REQUESTED, PREPARATIONS_RETRIEVED, CLEAR_PREPARATIONS,
+    GUIDANCE_CREATED, DIET_CREATED, MENU_CREATED
 } from '../actions/types';
 
 const INITIAL_STATE = {
-    timelinePrep: [],
-    guidelinePrep: [],
+    preparations: null,
+    guidancePrep: [],
     dietPrep: [],
     menuPrep: [],
     loading: false,
@@ -17,19 +17,12 @@ export default (state = INITIAL_STATE, action) => {
         case PREPARATIONS_REQUESTED:
             console.log('PREPARATIONS are being retrieved');
             return {...state, loading: true};
-        
-        case TIMELINE_CREATED:
-            console.log('timeline has been updated: ' + action.payload);
-            return {
-                ...state, 
-                timelinePrep: action.payload, 
-            };
 
-        case GUIDELINE_CREATED:
-            console.log('guideline has been updated: ' + action.payload);
+        case GUIDANCE_CREATED:
+            console.log('guidance has been updated: ' + action.payload);
             return {
                 ...state, 
-                guidelinePrep: action.payload, 
+                guidancePrep: action.payload, 
             };
 
         case DIET_CREATED:
@@ -40,15 +33,15 @@ export default (state = INITIAL_STATE, action) => {
             };
 
         case MENU_CREATED:
-            console.log('diet has been updated: ' + action.payload);
+            console.log('menu has been updated: ' + action.payload);
             return {
                 ...state, 
                 menuPrep: action.payload, 
             };
 
-        case PREPARATIONS_COMPLETE:
-            console.log('preparations have completed and all info is split');
-            return {...state, loading: false};
+        case PREPARATIONS_RETRIEVED:
+            console.log(`preparations have completed: ${JSON.stringify(action.payload)}`);
+            return {...state, loading: false, preparations: action.payload};
 
         case CLEAR_PREPARATIONS:
             return {...INITIAL_STATE};

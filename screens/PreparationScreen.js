440696647f8dc92009578f574b9aa0278c66a216
k1763447 <ahmed.1.ali@kcl.ac.uk>
2019-03-27 02:58:45 +0000

    Add React Render to Guidance

--- screens/PreparationScreen.js
+++ screens/PreparationScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {
+  Text,
   View,
   StatusBar
 } from 'react-native';
 import GuidanceList from '../components/GuidanceList';
+import ReactRender from '../components/common/ReactRender/ReactRender';
 
 export default class PreparationScreen extends React.Component {
 
@@ -16,7 +18,35 @@ export default class PreparationScreen extends React.Component {
       <View style={{ margin: 5 }}>
       <StatusBar barStyle="light-content" />
         <View>
-          <GuidanceList />
+        <Text style={{
+                  fontSize: 25, 
+                  paddingLeft: 10, 
+                  paddingBottom: 20, 
+                  paddingTop: 30, 
+                  fontWeight: 'bold', 
+                  color: 'black'}}
+                >Ingredients</Text>
+      
+                {/* THIS IS WHERE WE BULLET POINT INGREDIENTS IN A FLATLIST */}
+                <ReactRender 
+                style={{height: undefined, width: undefined}}
+                data={require('./IngredientsTESTTemp.json')}
+                />
+
+                <Text style={{
+                  fontSize: 25, 
+                  paddingLeft: 10, 
+                  paddingBottom: 20, 
+                  paddingTop: 30, 
+                  fontWeight: 'bold', 
+                  color: 'black'}}
+                >How to cook</Text>
+                  
+              {/* This is where the code for rendering methods is: */}
+              <ReactRender 
+                style={{height: undefined, width: undefined}}
+                data={require('./FoodTESTTemp.json')}
+                />
         </View>
       </View>      
     );
